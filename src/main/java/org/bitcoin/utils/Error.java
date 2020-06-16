fa8cbdde6e2db078d1ff3d8df0e9f42fee4708bc
Natane <natane-b@hotmail.fr>
2020-06-16 14:43:20 +0200

    Ajout de la classe des codes d'erreurs

---
+++ src/main/java/org/bitcoin/utils/Error.java
@@ -0,0 +1,18 @@
+package org.bitcoin.utils;
+
+/**
+ * Classe d'erreurs
+ * @author Natane Bendavid
+ */
+public class Error {
+    // Other
+    public static final int ERROR_INTERNAL_CODE = 199;
+    public static final String ERROR_INTERNAL_MSG = "Internal error";
+
+    // Logger
+    public static final int ERROR_LOGGER_DIR_FILES_LIST = 500;
+    public static final int ERROR_LOGGER_DIR_CONTAINS_BAD_FILE = 501;
+    public static final int ERROR_LOGGER_PARSE_LOGS_FILE_DATE = 502;
+    public static final int ERROR_LOGGER_REMOVE_FILE = 503;
+}
+
