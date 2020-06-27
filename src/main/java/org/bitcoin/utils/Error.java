d0e1a3be21b36d468b75c5fead42166773676a07
Natane <natane-b@hotmail.fr>
2020-06-28 01:32:08 +0200

    Ajout des erreurs user

--- src/main/java/org/bitcoin/utils/Error.java
+++ src/main/java/org/bitcoin/utils/Error.java
@@ -9,6 +9,18 @@ public class Error {
     public static final int ERROR_INTERNAL_CODE = 199;
     public static final String ERROR_INTERNAL_MSG = "Internal error";
 
+    // USER
+    public static final int ERROR_CHECK_LOGIN_CODE = 200;
+    public static final String ERROR_CHECK_LOGIN_MSG = "Login ou mot de passe incorrecte";
+    public static final int ERROR_CHECK_USER_DATA_CODE = 201;
+    public static final String ERROR_CHECK_USER_DATA_MSG = "Les données de l'utilsateur sont manquantes";
+    public static final int ERROR_CHECK_USER_MAIL_CODE = 201;
+    public static final String ERROR_CHECK_USER_MAIL_MSG = "L'adresse mail est incorrecte";
+    public static final int ERROR_CHECK_USER_MAIL_EXIST_CODE = 201;
+    public static final String ERROR_CHECK_USER_MAIL_EXIST_MSG = "L'adresse mail existe déja";
+    public static final int ERROR_CHECK_USER_LOGIN_EXIST_CODE = 201;
+    public static final String ERROR_CHECK_USER_LOGIN_EXIST_MSG = "Le login existe déja";
+
     // Logger
     public static final int ERROR_LOGGER_DIR_FILES_LIST = 500;
     public static final int ERROR_LOGGER_DIR_CONTAINS_BAD_FILE = 501;
