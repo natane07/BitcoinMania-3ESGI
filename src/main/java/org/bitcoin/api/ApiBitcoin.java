9971b3bb9e93c3f3fd941605a8e5238fb4d83f73
Natane <natane-b@hotmail.fr>
2020-06-29 22:42:08 +0200

    Ajout de l'api pour récupérer le cour du bitcoin

--- src/main/java/org/bitcoin/api/ApiBitcoin.java
+++ src/main/java/org/bitcoin/api/ApiBitcoin.java
@@ -1,7 +1,16 @@
 package org.bitcoin.api;
 
+import org.bitcoin.app.App;
 import org.bitcoin.http.HttpClient;
+import org.bitcoin.utils.Modal;
 import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * Classe pour l'api bitcoin
@@ -11,6 +20,8 @@ import org.json.JSONObject;
 public class ApiBitcoin {
     private static final String apiUri = "https://api.coindesk.com/v1/bpi/currentprice.json";
     private static HttpClient http = HttpClient.getHttp().setBaseUri(apiUri);
+    private static JSONObject json;
+    private static JSONObject jsonCoindesk;
 
     public static void test() {
         String response = http.fetch("", "GET");
@@ -18,5 +29,22 @@ public class ApiBitcoin {
         JSONObject categories = json.getJSONObject("time");
         System.out.println(categories);
         System.out.println(categories.getString("updateduk"));
+    }
+
+    public static ArrayList<String> getAllExchange() {
+        final String apiUriBitcoin = "https://blockchain.info";
+        HttpClient httpBitcoin = HttpClient.getHttp().setBaseUri(apiUriBitcoin);
+        App.logger.debug("Call api: " + apiUriBitcoin);
+        String response = httpBitcoin.fetch("/ticker", "GET");
+        json = new JSONObject(response);
+        Iterator<String> keys = json.keys();
+        ArrayList<String> money = new ArrayList<>();
+        while(keys.hasNext()) {
+            String key = keys.next();
+            if (json.get(key) instanceof JSONObject) {
+                money.add(key);
+            }
+        }
+        return money;
     }
 }
